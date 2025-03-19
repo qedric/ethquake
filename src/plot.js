33c49ae8e7fae277a0daf1453dc58181f66d6079
Qedric <qedricjames@gmail.com>
2025-03-19 22:11:11 +0100

    updated plot

--- src/plot.js
+++ src/plot.js
@@ -2,6 +2,10 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+/*  usage
+  node src/plot.js transactions_by_address_addresses_of_interest_6pct.json
+*/
+
 // Get directory name in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
